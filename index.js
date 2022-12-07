d4d4a9524c6733c3bac280a123ca4904f741c535
nalyk <dev.ungheni@gmail.com>
2022-12-07 20:52:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ S3Bucket.label = "S3 Space";
 
 S3Bucket.prototype.clientGeneration = true;
 
-S3Bucket.events = ["post", "put", "get", "delete"];
+S3Bucket.events = ["post"];
 S3Bucket.basicDashboard = {
     settings: [{
         name: 'key'
@@ -56,37 +56,7 @@ S3Bucket.prototype.handle = function (ctx, next) {
 
     if (!this.s3) return ctx.done("Missing S3 configuration!");
 
-    if (req.method === "GET") {
-        if ( ctx.query.signedUrl == 'Put' ) {
-            if (this.events.put) {
-                this.events.put.run(ctx, domain, function(err) {
-                    if (err) return ctx.done(err);
-                    bucket.put(ctx, next);
-                });
-            } else {
-                this.put(ctx, next);
-            }
-        } else {
-            if (this.events.get) {
-                this.events.get.run(ctx, domain, function(err) {
-                    if (err) return ctx.done(err);
-                    bucket.get(ctx, next);
-                });
-            } else {
-                this.get(ctx, next);
-            }
-        }
-    } else if (req.method === "DELETE") {
-        if (this.events['delete']) {
-            this.events['delete'].run(ctx, domain, function(err) {
-                if (err) return ctx.done(err);
-                bucket.delete(ctx, next);
-            });
-        } else {
-            this.delete(ctx, next);
-        }
-    } else if (req.method === "POST") {
-        
+    if (req.method === "POST") {    
         if (this.events['post']) {
             this.events['post'].run(ctx, domain, function(err) {
                 if (err) return ctx.done(err);
@@ -108,82 +78,28 @@ S3Bucket.prototype.post = function (ctx, next) {
     ctx.body = {};
     
     var form = new formidable.IncomingForm(),
-        uploadDir = '/tmp',
-        resultFiles = [],
-        remainingFile = 0;
+        uploadDir = '/tmp';
+
+    var formFileInfo = {};
 
     form.uploadDir = uploadDir;
 
-    var processDone = function(err, fields, files) {
-        if (err) return ctx.done(err);
-
-        console.log('fields')
-        console.log(fields)
-
-        console.log('files')
-        console.log(files['files[]'])
-
-        return ctx.done({ statusCode: 200, message: "var processDone - END cb" });
-    };
-    
-    form.parse(req, processDone);
-    req.resume();
-
-    // console.log(resultFiles);
-    return ctx.done({ statusCode: 200, message: "S3Bucket.prototype.post - END cb" });
-}
-
-// get a signedUrl for get object into s3
-S3Bucket.prototype.get = function (ctx, next) {
-    // remove the first /
-    var s3Key = ctx.url[0] == '/' ? ctx.url.substr(1) : ctx.url;
-
-    var params = {
-        Bucket: this.config.bucket,
-        Key: s3Key,
-        Expires: ctx.query.Expires,
-    };
-
-    this.s3.getSignedUrl('getObject', params, function(err, url){
-        if (err) {
-            return ctx.done(err);
-        }
-        if (ctx.query.returnFormat == 'Url') {
-            // simple ajax to get url link
-            ctx.done(null, url);
-        } else {
-            // redirect (can be used in <img src="/s3bucket/apple.jpg">)
-            ctx.res.statusCode = 307;
-            ctx.res.setHeader("Location", url);
-            ctx.res.end(null, url);
-        }
-    });
-}
-
-// get a signedUrl for put object into s3
-S3Bucket.prototype.put = function (ctx, next) {
-    // remove the first /
-    var s3Key = ctx.url[0] == '/' ? ctx.url.substr(1) : ctx.url;
-
-    var params = {
-        Bucket: this.config.bucket,
-        Key: s3Key,
-        Expires: ctx.query.Expires || 5*60, // default 5*60 seconds
-        ContentType: ctx.query.ContentType, // required
-    };
-    if (ctx.query.ContentLength) {
-        params.ContentLength = ctx.query.ContentLength;
+    var s3UploadProcessed = function(fileInfo) {
+        console.log('function s3UploadProcessed');
+        console.log(fileInfo);
+        return ctx.done(null, fileInfo);
     }
+    
+    form.parse(req)
+    .on('field', function(fieldName, fieldValue) {
+        console.log('field', { key: fieldName, fieldValue: fieldValue });
+    }).on('file', function(name, file) {
+        console.log('file', { name: name, file: file });
+    }).on('error', function(err) {
+        return ctx.done(err);
+    }).on('end', function() {
+        return s3UploadProcessed(formFileInfo);
+    });
 
-    this.s3.getSignedUrl('putObject', params, ctx.done);
-}
-
-// get a signedUrl for delete object into s3
-S3Bucket.prototype.delete = function (ctx, next) {
-    // remove the first /
-    var s3Key = ctx.url[0] == '/' ? ctx.url.substr(1) : ctx.url;
-    this.s3.deleteObject({
-        Bucket: this.config.bucket,
-        Key: s3Key,
-    }, ctx.done);
+    req.resume();
 }
